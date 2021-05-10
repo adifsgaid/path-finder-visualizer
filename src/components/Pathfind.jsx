58017ed070b7163cdf4cd5261d3a3065edb7e672
adifsgaid <infosgaid@gmail.com>
2021-05-10 16:52:57 +0200

    Itteratation for each cols

--- src/components/Pathfind.jsx
+++ src/components/Pathfind.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+const row = 5;
+const cols = 5;
 
 export default function Pathfind() {
+  const { grid, setGrid } = useState([]);
+
+  const gridInitializer = () => {
+    const grid = new Array(cols);
+
+    for (let i = 0; i < cols; i++) {
+      grid[i] = new Array(rows);
+    }
+  };
   return (
     <div>
       <h1> Pathfind Component</h1>
