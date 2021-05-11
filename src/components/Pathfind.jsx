e913bd4e33517fd16d702fadbce191515c4e620a
adifsgaid <infosgaid@gmail.com>
2021-05-11 16:16:43 +0200

    added node and imported css

--- src/components/Pathfind.jsx
+++ src/components/Pathfind.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
+import Node from "./Node";
+import "./Pathfind.css";
 const rows = 5;
 const cols = 5;
 
 export default function Pathfind() {
   const { grid, setGrid } = useState([]);
 
+  // We use useEffect to prioritaze the render of the  initializer in to the dom
+
+  useEffect(() => {
+    gridInitializer();
+  }, []); // This tells React that your effect doesn’t depend on any values from props or state, so it never needs to re-run.
+
+  // Grid Itteration To create a grid
   const gridInitializer = () => {
     const grid = new Array(cols);
 
@@ -12,8 +21,10 @@ export default function Pathfind() {
       grid[i] = new Array(rows);
     }
     createSpot(grid);
+    setGrid(grid);
   };
 
+  // Spot creation To find where the spot IS
   const createSpot = (grid) => {
     for (let i = 0; i < cols; i++) {
       for (let j = 0; j < rows; j++) {
@@ -30,6 +41,7 @@ export default function Pathfind() {
     this.h = 0;
   };
 
+  console.log(grid);
   return (
     <div>
       <h1> Pathfind Component</h1>
